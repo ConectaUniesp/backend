c7ea6ad81ce9989fa91667a032c4efd5fc728112
João Uchoa <joaovictoruchoa01@gmail.com>
2024-05-16 23:28:23 -0300

    Finalizando a task de recuperar a conta

--- develop/src/main/java/com/conectauniesp/conectauniesp/controller/AccountRecoverController.java
+++ develop/src/main/java/com/conectauniesp/conectauniesp/controller/AccountRecoverController.java
@@ -1,24 +1,2 @@
-@RestController
-@Autowired
-private UserService userService;
-
-@PostMapping("/forgot-password")
-public ResponseEntity<?> forgotPassword(@RequestParam String email) {
-        User user = userService.findByEmail(email);
-        if (user == null) {
-        return ResponseEntity.notFound().build();
-        }
-
-        // GERAR LINK PARA RECUPERAÇÃO DO EMAIL
-
-        return ResponseEntity.ok().build();
-        }
-
-@PostMapping("/reset-password")
-public ResponseEntity<?> resetPassword(@RequestParam String token, @RequestParam String newPassword) {
-        // Lógica para validar o token e redefinir a senha do usuário
-
-        return ResponseEntity.ok().build();
-        }
+package com.conectauniesp.conectauniesp.controller;public class AccountRecoverController {
 }
-//atualizado
